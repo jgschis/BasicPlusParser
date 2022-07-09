25b0677eaf7f986855afd1c8dd7b6a4d742cb1f3
jgschis <jgschism1054@gmail.com>
2022-07-10 01:42:39 +1000

    Fixed bug in nested if statements.

--- BasicPlusParser/Analyser/UnassignedVariableAnalyser.cs
+++ BasicPlusParser/Analyser/UnassignedVariableAnalyser.cs
@@ -19,24 +19,28 @@ namespace BasicPlusParser.Analyser
             _prog = prog;
         }
 
-        public (bool, HashSet<Token>) AnalyseCore(IEnumerable<Statement> statements, HashSet<Token> env = null)
+        public (bool, HashSet<Token>, HashSet<Token>) AnalyseCore(IEnumerable<Statement> statements, HashSet<Token> env = null)
         {
+            /*
+             * TODO explain why this works.
+             *
+             */
+
             bool branchReturns = false;
             // Contains variables that are definitivley assigned in the current block being analysed.
-            HashSet<Token> definiteLocalScope = new(new TokenEqualityComparer());
-            // Contains variables that are definitely assigned in the current block being analysed that came from another block.
-            HashSet<Token> definiteOuterScope = new(new TokenEqualityComparer());
-            // A block is the smallest unit of code that is not guaranteed to be executed. 
-            // For example, each branch of an if statement is a block.
+            HashSet<Token> definiteInLabel = new(new TokenEqualityComparer());
+            // Contains variables that are definitely assigned when returning from a gosub.
+            HashSet<Token> definiteFromGosub = new(new TokenEqualityComparer());
+         
 
             if (env != null)
             {
-                definiteLocalScope.UnionWith(env);
+                definiteInLabel.UnionWith(env);
             }
 
             foreach (Statement statement in statements)
             {
-                foreach (var err in statement.GetReferencedVars().Where(v => !definiteLocalScope.Contains(v)))
+                foreach (var err in statement.GetReferencedVars().Where(v => !definiteInLabel.Contains(v)))
                 {
                     // Don't report the same error twice.
                     if (!UnassignedVars.Any(x => object.ReferenceEquals(x.Item1, err)))
@@ -50,48 +54,49 @@ namespace BasicPlusParser.Analyser
                     }
                 }
 
-                definiteLocalScope.UnionWith(statement.GetAssignedVars());
+                definiteInLabel.UnionWith(statement.GetAssignedVars());
+
                 if (!branchReturns)
                 {
-                    definiteOuterScope.UnionWith(statement.GetAssignedVars());
+                    definiteFromGosub.UnionWith(statement.GetAssignedVars());
                 }
 
                 switch (statement)
                 {
                     case ReturnStatement:
-                        return (true, definiteOuterScope);
+                        return (true, definiteFromGosub, definiteInLabel);
                     case ThenElseStatement s:
-                        (var thenReturns, var thenVars) = AnalyseCore(s.Then, definiteLocalScope);
-                        (var elseReturns, var elseVars) = AnalyseCore(s.Else, definiteLocalScope);
-                        definiteOuterScope.UnionWith(thenVars.Intersect(elseVars,new TokenEqualityComparer()));
-                        if (!thenReturns && !elseReturns) definiteLocalScope.UnionWith(thenVars.Intersect(elseVars, new TokenEqualityComparer()));
-                        if (!elseReturns && thenReturns) definiteLocalScope.UnionWith(elseVars);
-                        if (elseReturns && !thenReturns) definiteLocalScope.UnionWith(thenVars);
-                        if (thenReturns && elseReturns) return (true, definiteOuterScope);
+                        (var thenReturns, var thenVars,var thenLocalVars) = AnalyseCore(s.Then, definiteInLabel);
+                        (var elseReturns, var elseVars, var elseLocalVars) = AnalyseCore(s.Else, definiteInLabel);
+                        definiteFromGosub.UnionWith(thenVars.Intersect(elseVars,new TokenEqualityComparer()));
+                        if (!thenReturns && !elseReturns) definiteInLabel.UnionWith(thenLocalVars.Intersect(elseLocalVars, new TokenEqualityComparer()));
+                        if (!elseReturns && thenReturns) definiteInLabel.UnionWith(elseLocalVars);
+                        if (elseReturns && !thenReturns) definiteInLabel.UnionWith(thenLocalVars);
+                        if (thenReturns && elseReturns) return (true, definiteFromGosub, definiteInLabel);
                         branchReturns |= thenReturns | elseReturns;
                         break;
                     case LoopRepeatStatement s:
-                       (var loopRepeatReturns, var loopRepeatVars )= AnalyseCore(s.Statements, definiteLocalScope);
-                        definiteOuterScope.UnionWith(loopRepeatVars);
+                       (var loopRepeatReturns, var loopRepeatVars, var loopLocalVars )= AnalyseCore(s.Statements, definiteInLabel);
+                        definiteFromGosub.UnionWith(loopRepeatVars);
                         if (!loopRepeatReturns)
                         {
-                            definiteLocalScope.UnionWith(loopRepeatVars);
+                            definiteInLabel.UnionWith(loopRepeatVars);
                         }
-                        if (loopRepeatReturns) return (true, definiteOuterScope);
+                        if (loopRepeatReturns) return (true, definiteFromGosub, definiteInLabel);
                         break;
                     case ForNextStatement s:
-                        (var forNextReturns, var forNextVars) = AnalyseCore(s.Statements, definiteLocalScope);
-                        definiteOuterScope.UnionWith(forNextVars);
+                        (var forNextReturns, var forNextVars, var forNextLocalVars) = AnalyseCore(s.Statements, definiteInLabel);
+                        definiteFromGosub.UnionWith(forNextVars);
                         if (!forNextReturns)
                         {
-                            definiteLocalScope.UnionWith(forNextVars);
+                            definiteInLabel.UnionWith(forNextVars);
                         }
-                        if (forNextReturns) return (true, definiteOuterScope);
+                        if (forNextReturns) return (true, definiteFromGosub, definiteInLabel);
                         break;
                     case CaseStmt s:
                         foreach (Case @case in s.Cases)
                         {
-                            AnalyseCore(@case.Statements, definiteLocalScope);
+                            AnalyseCore(@case.Statements, definiteInLabel);
                         }
                         break;
                     case GosubStatement s:
@@ -100,10 +105,10 @@ namespace BasicPlusParser.Analyser
                             if (_prog.SymbolTable.Labels.ContainsKey(s.Label.Name)){
                                 var stmtsAfterLabel = _prog.SymbolTable.Labels[s.Label.Name].StatementsFollowingLabel;
                                 JumpsTaken.Push(s.Label.Name);
-                                (var gosubReturns, var gosubVars) = AnalyseCore(stmtsAfterLabel, definiteLocalScope);
+                                (var gosubReturns, var gosubVars, var gosubLocalVars) = AnalyseCore(stmtsAfterLabel, definiteInLabel);
                                 JumpsTaken.Pop();
-                                definiteLocalScope.UnionWith(gosubVars);
-                                definiteOuterScope.UnionWith(gosubVars);  
+                                definiteInLabel.UnionWith(gosubVars);
+                                definiteFromGosub.UnionWith(gosubVars);  
                             }
                         }
                         break;
@@ -113,52 +118,57 @@ namespace BasicPlusParser.Analyser
                             if (_prog.SymbolTable.Labels.ContainsKey(s.Label.Name))
                             {
                                 JumpsTaken.Push(s.Label.Name);
-                                (var gotoReturns, var gotoVars) = AnalyseCore(_prog.SymbolTable.Labels[s.Label.Name].StatementsFollowingLabel, definiteLocalScope);
+                                (var gotoReturns, var gotoVars, _) = AnalyseCore(_prog.SymbolTable.Labels[s.Label.Name].StatementsFollowingLabel, definiteInLabel);
                                 JumpsTaken.Pop();
-                                // Goto always (effectively) returns...
                             }
                         }
-                        return (true, definiteOuterScope);
+                        return (true, definiteFromGosub, definiteInLabel);
 
                     case OnGosubStatement s:
                         HashSet<Token> onGosubVars = new(new TokenEqualityComparer());
                         bool firstTime = true;
                         foreach (var label in s.Labels)
                         {
-                            if (_prog.SymbolTable.Labels.ContainsKey(label.Name))
+                            if (!JumpsTaken.Contains(label.Name))
                             {
-                                JumpsTaken.Push(label.Name);
-                                (var gosubReturns, var gosubVars) = AnalyseCore(_prog.SymbolTable.Labels[label.Name].StatementsFollowingLabel, definiteLocalScope);
-                                JumpsTaken.Pop();
-                                if (firstTime)
+                                if (_prog.SymbolTable.Labels.ContainsKey(label.Name))
                                 {
-                                    firstTime = false;
-                                    onGosubVars.UnionWith(gosubVars);
-                                }
-                                else
-                                {
-                                    onGosubVars.IntersectWith(gosubVars);
+                                    JumpsTaken.Push(label.Name);
+                                    (var gosubReturns, var gosubVars, _) = AnalyseCore(_prog.SymbolTable.Labels[label.Name].StatementsFollowingLabel, definiteInLabel);
+                                    JumpsTaken.Pop();
+                                    if (firstTime)
+                                    {
+                                        firstTime = false;
+                                        onGosubVars.UnionWith(gosubVars);
+                                    }
+                                    else
+                                    {
+                                        onGosubVars.IntersectWith(gosubVars);
+                                    }
                                 }
                             }
                         }
-                        definiteLocalScope.UnionWith(onGosubVars);
-                        definiteOuterScope.UnionWith(onGosubVars);
+                        definiteInLabel.UnionWith(onGosubVars);
+                        definiteFromGosub.UnionWith(onGosubVars);
                         break;
                     case OnGotoStatement s:
                         foreach (var label in s.Labels)
                         {
-                            if (_prog.SymbolTable.Labels.ContainsKey(label.Name))
+                            if (!JumpsTaken.Contains(label.Name))
                             {
-                                JumpsTaken.Push(label.Name);
-                                AnalyseCore(_prog.SymbolTable.Labels[label.Name].StatementsFollowingLabel, definiteLocalScope);
-                                JumpsTaken.Pop();
+                                if (_prog.SymbolTable.Labels.ContainsKey(label.Name))
+                                {
+                                    JumpsTaken.Push(label.Name);
+                                    AnalyseCore(_prog.SymbolTable.Labels[label.Name].StatementsFollowingLabel, definiteInLabel);
+                                    JumpsTaken.Pop();
+                                }
                             }
                         }
-                        return (true, definiteOuterScope);
+                        return (true, definiteFromGosub, definiteInLabel);
 
                 }
             }
-            return (false, definiteOuterScope);
+            return (false, definiteFromGosub, definiteInLabel);
         }
 
         public void Analyse()
