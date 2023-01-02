c57c521d814076f47daa340662085d78eec9940f
jgschis <jgschism1054@gmail.com>
2023-01-02 22:24:26 +1100

    Added language server client

---
+++ BasicPlusLangClient/client/src/extension.ts
@@ -0,0 +1,58 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	const serverExe = 'dotnet';
+
+	const serverModule = context.asAbsolutePath(
+		path.join('.server', 'BasicPlusLangServer.dll')
+	);
+
+	const serverOptions: ServerOptions = {
+        run: { command: serverExe, args: [serverModule],transport: TransportKind.stdio,},
+        debug: { command: serverExe, args: [serverModule], transport: TransportKind.stdio,}
+    };
+
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'languageServerExample',
+		'Language Server Example',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
